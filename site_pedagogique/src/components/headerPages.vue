cdf9800e47124d339ad374366b6b09f622b300e3
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-05-03 10:55:14 +0200

    feat(header): ajout redirection

--- site_pedagogique/src/components/headerPages.vue
+++ site_pedagogique/src/components/headerPages.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="topPage">ici header</div>
+    <div class="topPage">
+      <img class="logo" src="../assets/logoWHT.png" @click="this.$router.push({ name: 'PageAccueil' })"/>
+      <div class="link" @click="this.$router.push({ name: 'PageAccueil' })">Retour à l'accueil</div>
+    </div>
   </template>
   
   <script>
@@ -13,12 +16,22 @@
     margin: 0 !important;
   }
   .topPage{
+    display: flex;
     position: fixed;
+    align-items: center;
+    justify-content: center;
     top:0;
     width: 100vw;
     background-color: black;
     color: white;
     height: 50px;
+  }
+  .logo{
+    width: 30px;
+    cursor: pointer;
+  }
+  .link{
+    cursor: pointer;
   }
   </style>
   
