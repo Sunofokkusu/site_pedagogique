ad9854a2670ea8ba2bf7b7ce72506bfb7596a238
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-05-10 11:40:24 +0200

    feat(couleurs): ajout oeuvres grises

--- site_pedagogique/src/components/pageGris.vue
+++ site_pedagogique/src/components/pageGris.vue
@@ -2,7 +2,48 @@
   <div>
     <headerPage />
     <div class="corps">
-      <h2>PHOTO GRISES</h2>
+      <h2>Qu'est ce que le gris?</h2>
+      <p class="def">
+        Le gris est une couleur intermédiaire entre le blanc et le noir.
+      </p>
+      <h2>Les oeuvres grises connues dans l'art</h2>
+      <div class="row">
+        <img
+          class="col-2"
+          src="https://static.nationalgeographic.fr/files/styles/image_3200/public/Main_Guernica_BAT-10313.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="http://ekladata.com/Nv41TVr_--zDZr98vW2ryfWRr3c@600x444.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="http://ekladata.com/Xbb0rKSjxGGGuoCd5_MO30KMsj0@600x486.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="http://ekladata.com/emO9BoDzcGYLTxm3H3xGwRyCC-s.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="http://ekladata.com/bjwBXqPz6xBtEDpoyg9jre3ARJY.jpg"
+        />
+      </div>
+      <div class="row">
+        <div class="col-2">Guernica, de Pablo Picasso</div>
+        <div class="col-2 img">L'arbre gris, de Piet Mondrian</div>
+        <div class="col-2 img">
+          Le cheval gris, de Théodore Géricault
+        </div>
+        <div class="col-2 img">
+          Girl in grey, de Louis Le Brocquy
+        </div>
+        <div class="col-2 img">
+          Maternité, de Nguyen Thanh Binh
+        </div>
+      </div>
+      <h2>A toi de jouer!</h2>
+      
       <div class="photosInfos" v-for="photo in jsonPhotosSorted" :key="photo">
         <div class="row">
           <img class="col-4" :src="getImg(photo.chemin)" />
