949ee75575bebb23b76da46ac3ca353adffbba1b
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-05-10 11:40:13 +0200

    feat(couluers): mélange de couleurs et def+oeuvres

--- site_pedagogique/src/components/pageBleu.vue
+++ site_pedagogique/src/components/pageBleu.vue
@@ -2,7 +2,65 @@
   <div>
     <headerPage />
     <div class="corps">
-      <h2>PHOTOS BLEUES</h2>
+      <h2>Qu'est ce que le bleu?</h2>
+      <p class="def">
+        Le bleu est une couleur primaire, c'est à dire une couleur qui ne peut pas être reproduite par un mélange d'autres couleurs.
+        C'est l'une des couleurs les plus courantes dans la nature et la vie quotidienne.
+      </p>
+      <h2>Les oeuvres bleues connues dans l'art</h2>
+      <div class="row">
+        <img
+          class="col-2"
+          src="https://www.artmajeur.com/media/cache/resolve/standard/o/l/olimpia-gaia-martinelli/blog/15504022-blueday.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="https://www.artmajeur.com/media/cache/resolve/standard/o/l/olimpia-gaia-martinelli/blog/15572881-dsc-0001.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="https://www.artmajeur.com/media/cache/resolve/standard/o/l/olimpia-gaia-martinelli/blog/15535954-7ee2fe3cfdceea977ad88356506c359.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg/1200px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="https://www.vangoghgallery.com/img/starry_night_full.jpg"
+        />
+      </div>
+      <div class="row">
+        <div class="col-2">Jour bleu, de Cfey</div>
+        <div class="col-2 img">Depuis un endroit élevé, de Stuart Dalby</div>
+        <div class="col-2 img">
+          Perception bleue 2, de Weilong Chen
+        </div>
+        <div class="col-2 img">
+          Self-Portrait, de Vincent van Gogh
+        </div>
+        <div class="col-2 img">Starry Night, de Vincent van Gogh</div>
+      </div>
+      <h2>A toi de jouer!</h2>
+      <p>Mélange des couleurs avec le bleu pour voir le résultat.</p>
+      <div v-if="!melange">
+        <div class="row">
+          <div class="col-2 ble"></div>
+          <div class="col-2" :class="color"></div>
+          <div class="col-1">
+            <div class="choixR" @click="color='roug'"></div>
+          <div class="choixJ" @click="color='jaun'"></div>
+          <div class="choixW" @click="color='white'"></div>
+          </div>
+        </div>
+        <button @click="melanger" class="boutton">Mélanger</button>
+      </div>
+      <div v-else>
+        <div class="row">
+          <div class="col-2" :class="colorMelange"></div>
+        </div>
+        <button @click="reset" class="boutton">Retour au choix des couleurs</button>
+      </div>
       <div class="photosInfos" v-for="photo in jsonPhotosSorted" :key="photo">
         <div class="row">
           <img class="col-4" :src="getImg(photo.chemin)" />
@@ -34,6 +92,9 @@ export default {
     return {
       jsonPhotos: myJSON,
       jsonPhotosSorted: [],
+      melange: false,
+      color: "white",
+      colorMelange: ""
     };
   },
   mounted() {
@@ -48,6 +109,21 @@ export default {
     getImg(img) {
       return require(`../assets/photos/${img}`);
     },
+    melanger(){
+      this.melange = true;
+      if(this.color === "white"){
+        this.colorMelange = "bleuC"
+      }else if(this.color === "jaun"){
+        this.colorMelange = "vert"
+      }else if(this.color === "roug"){
+        this.colorMelange = "violet"
+      }
+    },
+    reset(){
+      this.color = "white";
+      this.colorMelange = "";
+      this.melange = false;
+    }
   },
 };
 </script>
@@ -67,6 +143,31 @@ body {
   align-items: center;
   justify-content: center;
   font-size: 15px;
+}
+.choixJ{
+  cursor: pointer;
+  margin-left: 20px;
+  height: 5vh !important;
+  border: 1px solid black;
+  margin-bottom: 10px;
+  background-color: yellow;
+}
+.jaun{
+  margin-left: 20px;
+}
+.bleuC{
+  height: 20vh !important;
+  border: 1px solid black;
+  margin-left: 20px;
+  background-color: lightblue;
+  margin-bottom: 30px;
+}
+.violet{
+  height: 20vh !important;
+  border: 1px solid pruple;
+  margin-left: 20px;
+  background-color: purple;
+  margin-bottom: 30px;
 }
 .next {
   margin-left: 5%;
--- site_pedagogique/src/components/pageJaune.vue
+++ site_pedagogique/src/components/pageJaune.vue
@@ -2,7 +2,65 @@
   <div>
     <headerPage />
     <div class="corps">
-      <h2>PHOTOS JAUNES</h2>
+      <h2>Qu'est ce que le jaune?</h2>
+      <p class="def">
+        Le jaune est une couleur primaire, c'est à dire une couleur qui ne peut pas être reproduite par un mélange d'autres couleurs.
+        C'est une couleur lumineuse et chaleureuse. C’est la couleur du soleil, de la lumière et de la gaieté.
+      </p>
+      <h2>Les oeuvres jaunes connues dans l'art</h2>
+      <div class="row">
+        <img
+          class="col-2"
+          src="https://perezartsplastiques.files.wordpress.com/2017/09/n00544_10.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="https://perezartsplastiques.files.wordpress.com/2017/09/douze.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="https://perezartsplastiques.files.wordpress.com/2017/09/abf89ec794471712162f236bfe0eaee2.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="https://perezartsplastiques.files.wordpress.com/2017/09/tumblr_m75d71r7bw1qbo39mo1_1280.jpg"
+        />
+        <img
+          class="col-2 img"
+          src="https://perezartsplastiques.files.wordpress.com/2017/09/mark_rothko.jpg"
+        />
+      </div>
+      <div class="row">
+        <div class="col-2">Going to the Ball, de Turner</div>
+        <div class="col-2 img">Les tournesols, de Van Gogh</div>
+        <div class="col-2 img">
+          Le phare de Collioure, de André Derain
+        </div>
+        <div class="col-2 img">
+          Le baiser, de Gustav Klimt
+        </div>
+        <div class="col-2 img">Jaune orange rouge, de Mark Rothko</div>
+      </div>
+      <h2>A toi de jouer!</h2>
+      <p>Mélange des couleurs avec le jaune pour voir le résultat.</p>
+      <div v-if="!melange">
+        <div class="row">
+          <div class="col-2 jaun"></div>
+          <div class="col-2" :class="color"></div>
+          <div class="col-1">
+            <div class="choixR" @click="color='roug'"></div>
+          <div class="choixB" @click="color='ble'"></div>
+          <div class="choixW" @click="color='white'"></div>
+          </div>
+        </div>
+        <button @click="melanger" class="boutton">Mélanger</button>
+      </div>
+      <div v-else>
+        <div class="row">
+          <div class="col-2" :class="colorMelange"></div>
+        </div>
+        <button @click="reset" class="boutton">Retour au choix des couleurs</button>
+      </div>
       <div class="photosInfos" v-for="photo in jsonPhotosSorted" :key="photo">
         <div class="row">
           <img class="col-4" :src="getImg(photo.chemin)" />
@@ -34,6 +92,9 @@ export default {
     return {
       jsonPhotos: myJSON,
       jsonPhotosSorted: [],
+      melange: false,
+      color: "white",
+      colorMelange: ""
     };
   },
   mounted() {
@@ -48,6 +109,21 @@ export default {
     getImg(img) {
       return require(`../assets/photos/${img}`);
     },
+    melanger(){
+      this.melange = true;
+      if(this.color === "white"){
+        this.colorMelange = "jauneC"
+      }else if(this.color === "ble"){
+        this.colorMelange = "vert"
+      }else if(this.color === "roug"){
+        this.colorMelange = "orange"
+      }
+    },
+    reset(){
+      this.color = "white";
+      this.colorMelange = "";
+      this.melange = false;
+    }
   },
 };
 </script>
@@ -67,6 +143,73 @@ body {
   align-items: center;
   justify-content: center;
   font-size: 15px;
+}
+.jaun{
+  height: 20vh !important;
+  border: 1px solid black;
+  margin-bottom: 30px;
+  background-color: yellow;
+}
+.white{
+  height: 20vh !important;
+  border: 1px solid black;
+  margin-left: 20px;
+}
+.roug{
+  height: 20vh !important;
+  border: 1px solid black;
+  margin-left: 20px;
+  background-color: red;
+}
+.ble{
+  height: 20vh !important;
+  border: 1px solid black;
+  margin-left: 20px;
+  background-color: blue;
+  margin-bottom: 30px;
+}
+.jauneC{
+  height: 20vh !important;
+  border: 1px solid black;
+  margin-left: 20px;
+  background-color: lightyellow;
+  margin-bottom: 30px;
+}
+.vert{
+  height: 20vh !important;
+  border: 1px solid black;
+  margin-left: 20px;
+  background-color: green;
+  margin-bottom: 30px;
+}
+.orange{
+  height: 20vh !important;
+  border: 1px solid black;
+  margin-left: 20px;
+  background-color: orange;
+  margin-bottom: 30px;
+}
+.boutton, .choixB, .choixR, .choixW{
+  cursor: pointer;
+  margin-bottom: 10px;
+}
+.choixB{
+  height: 5vh !important;
+  border: 1px solid black;
+  margin-left: 20px;
+  background-color: blue;
+}
+.choixR{
+  height: 5vh !important;
+  border: 1px solid black;
+  margin-left: 20px;
+  background-color: red;
+}
+.choixW{
+  height: 5vh !important;
+  border: 1px solid black;
+  margin-left: 20px;
+  background-color: white;
 }
 .next {
   margin-left: 5%;
