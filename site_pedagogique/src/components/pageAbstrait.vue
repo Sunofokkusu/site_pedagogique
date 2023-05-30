1773b4a68de7f38449bad4d76f33860512566f1b
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-05-30 09:06:17 +0200

    fix(prettier): mise en page

--- site_pedagogique/src/components/pageAbstrait.vue
+++ site_pedagogique/src/components/pageAbstrait.vue
@@ -10,26 +10,11 @@
       </p>
       <h2>Les art abstraits connus dans l'art</h2>
       <div class="row">
-        <img
-          class="col-2"
-          src="../assets/exemples/sanstitre.jpg"
-        />
-        <img
-          class="col-2 img"
-          src="../assets/exemples/congress.jpg"
-        />
-        <img
-          class="col-2 img"
-          src="../assets/exemples/abstract.jpg"
-        />
-        <img
-          class="col-2 img"
-          src="../assets/exemples/composition.jpg"
-        />
-        <img
-          class="col-2 img"
-          src="../assets/exemples/cytwombly.jpg"
-        />
+        <img class="col-2" src="../assets/exemples/sanstitre.jpg" />
+        <img class="col-2 img" src="../assets/exemples/congress.jpg" />
+        <img class="col-2 img" src="../assets/exemples/abstract.jpg" />
+        <img class="col-2 img" src="../assets/exemples/composition.jpg" />
+        <img class="col-2 img" src="../assets/exemples/cytwombly.jpg" />
       </div>
       <div class="row">
         <div class="col-2">Sans titre, de Zao Wou-Ki</div>
@@ -41,10 +26,29 @@
         <div class="col-2 img">Sans titre, de Cy Twombly</div>
       </div>
       <h2>A toi de jouer!</h2>
-      <p>Dessine ton propre art abstrait.<br/>Couleur:&nbsp;<input type="color" v-model="color" /></p>
-        <vue-drawing-canvas :width="widthCanva" :color="color" ref="VueCanvasDrawing" />
-        <br/>
-      <button @click.prevent="$refs.VueCanvasDrawing.reset()" style="cursor: pointer;">Supprimer</button>
+      <p>
+        Dessine ton propre art abstrait.<br />Couleur:&nbsp;<input
+          type="color"
+          v-model="color"
+        />
+      </p>
+      <div id="toPdf">
+        <vue-drawing-canvas
+          :width="widthCanva"
+          :color="color"
+          ref="VueCanvasDrawing"
+        />
+        <br />
+      </div>
+      <div class="row">
+        <button
+          @click.prevent="$refs.VueCanvasDrawing.reset()"
+          style="cursor: pointer"
+        >
+          Supprimer
+        </button>
+        <button style="cursor: pointer;margin-left: 10px" @click="exportToPDF">Exporter en PDF</button>
+      </div>
       <h2>Les photos abstraites de l'exposition</h2>
       <div class="photosInfos" v-for="photo in jsonPhotosSorted" :key="photo">
         <div class="row">
@@ -75,6 +79,7 @@ import VueDrawingCanvas from "vue-drawing-canvas";
 import headerPage from "@/components/headerPages.vue";
 import footerPage from "@/components/footerPage.vue";
 import myJSON from "@/assets/photos.json";
+import html2pdf from "html2pdf.js";
 export default {
   name: "pageAbstrait",
   components: { headerPage, footerPage, VueDrawingCanvas },
@@ -83,12 +88,12 @@ export default {
       jsonPhotos: myJSON,
       jsonPhotosSorted: [],
       color: "#000000",
-      widthCanva: ""
+      widthCanva: "",
     };
   },
   mounted() {
     window.scroll(0, 0);
-    this.widthCanva = window.innerWidth/2;
+    this.widthCanva = window.innerWidth / 2;
     this.jsonPhotos.photographies.forEach((photo) => {
       if (photo.theme === "abstrait") {
         this.jsonPhotosSorted.push(photo);
@@ -98,6 +103,12 @@ export default {
   methods: {
     getImg(img) {
       return require(`../assets/photos/${img}`);
+    },
+    exportToPDF() {
+      html2pdf(document.getElementById("toPdf"), {
+        margin: 3,
+        filename: "abstrait.pdf",
+      });
     },
   },
 };
@@ -126,7 +137,7 @@ body {
 .prev {
   cursor: pointer;
 }
-#VueDrawingCanvas{
+#VueDrawingCanvas {
   border: 2px solid black;
 }
 </style>
--- site_pedagogique/src/components/pagePaysage.vue
+++ site_pedagogique/src/components/pagePaysage.vue
@@ -10,26 +10,11 @@
       </p>
       <h2>Les paysages connus dans l'art</h2>
       <div class="row">
-        <img
-          class="col-2"
-          src="../assets/exemples/nuit.jpg"
-        />
-        <img
-          class="col-2 img"
-          src="../assets/exemples/soleil.jpg"
-        />
-        <img
-          class="col-2 img"
-          src="../assets/exemples/moine.png"
-        />
-        <img
-          class="col-2 img"
-          src="../assets/exemples/canal.jpg"
-        />
-        <img
-          class="col-2 img"
-          src="../assets/exemples/estaque.jpg"
-        />
+        <img class="col-2" src="../assets/exemples/nuit.jpg" />
+        <img class="col-2 img" src="../assets/exemples/soleil.jpg" />
+        <img class="col-2 img" src="../assets/exemples/moine.png" />
+        <img class="col-2 img" src="../assets/exemples/canal.jpg" />
+        <img class="col-2 img" src="../assets/exemples/estaque.jpg" />
       </div>
       <div class="row">
         <div class="col-2">La nuit étoilée, de Vincent van Gogh</div>
@@ -43,16 +28,43 @@
         <div class="col-2 img">L’Estaque, de André Derain</div>
       </div>
       <h2>A toi de jouer!</h2>
-      <p>Crée  ton propre paysage en choisissant son contenu.</p>
-      <div v-if="ok" class="mainCanvas">
+      <p>Crée ton propre paysage en choisissant son contenu.</p>
+      <div v-if="ok" class="mainCanvas" id="toPdf">
         <img :src="getImgPays(tab1[iHaut])" class="overlayImage" />
         <img :src="getImgPays(tab2[iMilieu])" class="overlayImage" />
         <img :src="getImgPays(tab3[iBas])" class="overlayImage" />
       </div>
       <div class="row">
-        <div @click="changeBas('gauche')" class="btn"><img src="../assets/arrowLeft.png" class="arrow"/></div><div class="col-2 choix"><img :src="getImgPays(tab3[iBas])" class="previewImg" /></div><div @click="changeBas('droite')" class="btn"><img src="../assets/arrowRight.png" class="arrow"/></div>
-        <div @click="changeMilieu('gauche')" class="btn"><img src="../assets/arrowLeft.png" class="arrow arrowL"/></div><div class="col-2 choix choixL"><img :src="getImgPays(tab2[iMilieu])" class="previewImg" /></div><div @click="changeMilieu('droite')" class="btn"><img src="../assets/arrowRight.png" class="arrow"/></div>
-        <div @click="changeHaut('gauche')" class="btn"><img src="../assets/arrowLeft.png" class="arrow arrowL"/></div><div class="col-2 choix choixL"><img :src="getImgPays(tab1[iHaut])" class="previewImg" /></div><div @click="changeHaut('droite')" class="btn"><img src="../assets/arrowRight.png" class="arrow"/></div>
+        <div @click="changeBas('gauche')" class="btn">
+          <img src="../assets/arrowLeft.png" class="arrow" />
+        </div>
+        <div class="col-2 choix">
+          <img :src="getImgPays(tab3[iBas])" class="previewImg" />
+        </div>
+        <div @click="changeBas('droite')" class="btn">
+          <img src="../assets/arrowRight.png" class="arrow" />
+        </div>
+        <div @click="changeMilieu('gauche')" class="btn">
+          <img src="../assets/arrowLeft.png" class="arrow arrowL" />
+        </div>
+        <div class="col-2 choix choixL">
+          <img :src="getImgPays(tab2[iMilieu])" class="previewImg" />
+        </div>
+        <div @click="changeMilieu('droite')" class="btn">
+          <img src="../assets/arrowRight.png" class="arrow" />
+        </div>
+        <div @click="changeHaut('gauche')" class="btn">
+          <img src="../assets/arrowLeft.png" class="arrow arrowL" />
+        </div>
+        <div class="col-2 choix choixL">
+          <img :src="getImgPays(tab1[iHaut])" class="previewImg" />
+        </div>
+        <div @click="changeHaut('droite')" class="btn">
+          <img src="../assets/arrowRight.png" class="arrow" />
+        </div>
+      </div>
+      <div class="row">
+        <button class="boutton" @click="exportToPDF">Exporter en PDF</button>
       </div>
       <h2>Les paysages de l'exposition</h2>
       <div class="photosInfos" v-for="photo in jsonPhotosSorted" :key="photo">
@@ -83,6 +95,7 @@
 import headerPage from "@/components/headerPages.vue";
 import footerPage from "@/components/footerPage.vue";
 import myJSON from "@/assets/photos.json";
+import html2pdf from "html2pdf.js";
 export default {
   name: "pagePaysage",
   components: { headerPage, footerPage },
@@ -91,7 +104,7 @@ export default {
       jsonPhotos: myJSON,
       jsonPhotosSorted: [],
       tab1: ["ciel1.png", "ciel2.png", "ciel3.png"],
-      tab2: ["milieu1.png", "milieu2.png", "milieu3.png","milieu4.png"],
+      tab2: ["milieu1.png", "milieu2.png", "milieu3.png", "milieu4.png"],
       tab3: ["bas1.png", "bas2.png", "bas3.png"],
       iBas: 0,
       iMilieu: 0,
@@ -117,48 +130,54 @@ export default {
     },
     changeHaut(direction) {
       if (direction === "gauche") {
-        if(this.iHaut === 0){
-          this.iHaut = 2
-        }else{
+        if (this.iHaut === 0) {
+          this.iHaut = 2;
+        } else {
           this.iHaut -= 1;
         }
       } else if (direction === "droite") {
-        if(this.iHaut === 2){
-          this.iHaut = 0
-        }else{
+        if (this.iHaut === 2) {
+          this.iHaut = 0;
+        } else {
           this.iHaut += 1;
         }
       }
     },
     changeMilieu(direction) {
       if (direction === "gauche") {
-        if(this.iMilieu === 0){
-          this.iMilieu = 3
-        }else{
+        if (this.iMilieu === 0) {
+          this.iMilieu = 3;
+        } else {
           this.iMilieu -= 1;
         }
       } else if (direction === "droite") {
-        if(this.iMilieu === 3){
-          this.iMilieu = 0
-        }else{
+        if (this.iMilieu === 3) {
+          this.iMilieu = 0;
+        } else {
           this.iMilieu += 1;
         }
       }
     },
     changeBas(direction) {
       if (direction === "gauche") {
-        if(this.iBas === 0){
-          this.iBas = 2
-        }else{
+        if (this.iBas === 0) {
+          this.iBas = 2;
+        } else {
           this.iBas -= 1;
         }
       } else if (direction === "droite") {
-        if(this.iBas === 2){
-          this.iBas = 0
-        }else{
+        if (this.iBas === 2) {
+          this.iBas = 0;
+        } else {
           this.iBas += 1;
         }
       }
+    },
+    exportToPDF() {
+      html2pdf(document.getElementById("toPdf"), {
+        margin: 10,
+        filename: "paysage.pdf",
+      });
     },
   },
 };
@@ -192,17 +211,20 @@ body {
   position: absolute;
   top: 0;
 }
-.arrow{
+.arrow {
   width: 4vw;
 }
-.arrowL{
+.choix{
+  margin-bottom: 20px;
+}
+.arrowL {
   margin-left: 30px;
 }
-.previewImg{
+.previewImg {
   width: 15vw;
   border: 1px solid black;
 }
-.btn{
+.btn {
   display: flex;
   cursor: pointer;
   align-items: center;
