b0dda86ceaac632438b7e540aa9121a188bbe1cd
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-05-31 09:06:07 +0200

    feat(routes): nouvelle route

--- site_pedagogique/src/router/index.js
+++ site_pedagogique/src/router/index.js
@@ -4,11 +4,12 @@ import notFound from '@/components/notFound.vue';
 import pagePortrait from '@/components/pagePortrait.vue';
 import pagePaysage from '@/components/pagePaysage.vue';
 import pageAbstrait from '@/components/pageAbstrait.vue';
-import allPhotos from '@/components/allPhotos.vue';
+// import allPhotos from '@/components/allPhotos.vue';
 import pageJaune from '@/components/pageJaune.vue';
 import pageBleu from '@/components/pageBleu.vue';
 import pageNoir from '@/components/pageNoir.vue';
 import pageGris from '@/components/pageGris.vue';
+import pageNatureMorte from '@/components/pageNatureMorte.vue'
 
 const routes = [
    { name: "PageAccueil", path: "/", component: PageAccueil },
@@ -16,7 +17,8 @@ const routes = [
    { name: "pagePortrait", path: "/portrait", component: pagePortrait},
    { name: "pagePaysage", path: "/paysage", component: pagePaysage},
    { name: "pageAbstrait", path: "/abstrait", component: pageAbstrait},
-   { name: "allPhotos", path: "/all", component: allPhotos},
+   { name: "pageNatureMorte", path: "/natureMorte", component: pageNatureMorte},
+ //{ name: "allPhotos", path: "/all", component: allPhotos},
    { name: "pageJaune", path: "/jaune", component: pageJaune},
    { name: "pageBleu", path: "/bleu", component: pageBleu},
    { name: "pageNoir", path: "/noir", component: pageNoir},
