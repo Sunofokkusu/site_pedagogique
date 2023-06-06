7db21389799b514db8fe5a9fbe9b2674706b857e
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-06-06 15:19:07 +0200

    feat(activite): activité nature morte (A FIX)

--- site_pedagogique/src/components/activities/activiteNatureMorte.vue
+++ site_pedagogique/src/components/activities/activiteNatureMorte.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <h2>A toi de jouer!</h2>
+    <div style="width: 400px;height: 300px;">
+      <slide-puzzle :ratio="3" mode="near" />
+    </div>
   </div>
 </template>
 
 <script>
+import 'vue-slide-puzzle/dist/VueSlidePuzzle.css';
 export default {
   name: "activiteNatureMorte",
-  mounted() {
-  },
+  methods: {
+    getImg(img) {
+      return require(`../../assets/photos/${img}`);
+    },
+  }
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
