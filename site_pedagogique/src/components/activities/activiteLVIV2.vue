eb76019c5fc8f4c07358dce4e2abe7441d02e341
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-06-13 14:33:27 +0200

    feat(activite): activité LVIV2

---
+++ site_pedagogique/src/components/activities/activiteLVIV2.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+      <div class="row">
+        <img class="col-4" src="../../assets/photos/LVIV5.png" />
+        <div class="col-6">
+          <h2>LVIV</h2>
+          <p>Viacheslav POLIAKOV</p>
+          <p>Passe ta souris sur les carrés colorés pour découvrir leur couleur complémentaires!</p>
+          <div class="align">
+            <div class="jauneLVIV">
+            <q-tooltip anchor="center middle" self="center middle">
+              <div class="bleuLVIV"></div>
+            </q-tooltip>
+          </div>
+          <div class="bleuFLVIV">
+            <q-tooltip anchor="center middle" self="center middle">
+              <div class="jauneFLVIV"></div>
+            </q-tooltip>
+          </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "activiteLVIV",
+    methods: {},
+  };
+  </script>
+  
+  <style>
+  .align{
+    display: flex;
+    margin-left: 30%;
+  }
+  .jauneLVIV {
+    background-color: #dfcf32;
+    width: 10vw;
+    height: 10vh;
+    border: 1px solid black;
+  }
+  .bleuLVIV {
+    background-color: #3242df;
+    width: 9vw;
+    height: 9vh;
+    border: 1px solid black;
+  }
+  .bleuFLVIV{
+    background-color: #1C35B3;
+    width: 10vw;
+    height: 10vh;
+    border: 1px solid black;
+    margin-left: 15px !important;
+  }
+  .jauneFLVIV{
+    background-color: #b39a1c;
+    width: 9vw;
+    height: 9vh;
+    border: 1px solid black;
+  }
+  </style>
+  
