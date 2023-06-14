54381f2bf7c92520d42ec3a82a38954505031f46
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-06-14 13:57:46 +0200

    fix(activite): suppression couleur en double

--- site_pedagogique/src/components/activities/activiteLVIV.vue
+++ site_pedagogique/src/components/activities/activiteLVIV.vue
@@ -5,18 +5,16 @@
       <div class="col-6">
         <h2>LVIV</h2>
         <p>Viacheslav POLIAKOV</p>
-        <p>Passe ta souris sur les carrés colorés pour découvrir leur couleur complémentaire!</p>
+        <p>
+          Passe ta souris sur les carrés colorés pour découvrir leur couleur
+          complémentaire!
+        </p>
         <div class="align">
           <div class="jauneLVIV">
-          <q-tooltip anchor="center middle" self="center middle">
-            <div class="bleuLVIV"></div>
-          </q-tooltip>
-        </div>
-        <div class="bleuFLVIV">
-          <q-tooltip anchor="center middle" self="center middle">
-            <div class="jauneFLVIV"></div>
-          </q-tooltip>
-        </div>
+            <q-tooltip anchor="center middle" self="center middle">
+              <div class="bleuLVIV"></div>
+            </q-tooltip>
+          </div>
         </div>
       </div>
     </div>
@@ -32,9 +30,9 @@ export default {
 </script>
 
 <style>
-.align{
+.align {
   display: flex;
-  margin-left: 30%;
+  margin-left: 42%;
 }
 .jauneLVIV {
   background-color: #dfcf32;
@@ -44,19 +42,6 @@ export default {
 }
 .bleuLVIV {
   background-color: #3242df;
-  width: 9vw;
-  height: 9vh;
-  border: 1px solid black;
-}
-.bleuFLVIV{
-  background-color: #1C35B3;
-  width: 10vw;
-  height: 10vh;
-  border: 1px solid black;
-  margin-left: 15px !important;
-}
-.jauneFLVIV{
-  background-color: #b39a1c;
   width: 9vw;
   height: 9vh;
   border: 1px solid black;
