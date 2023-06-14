a21019b0f9af25deff993c7c9f51ffc775b5cc4a
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-06-14 15:14:57 +0200

    feat(js): import des icons nécessaires et de fontawesome

--- site_pedagogique/src/main.js
+++ site_pedagogique/src/main.js
@@ -3,5 +3,20 @@ import App from './App.vue'
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCamera } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faMountainSun } from '@fortawesome/free-solid-svg-icons'
+import { faWaveSquare } from '@fortawesome/free-solid-svg-icons'
+import { faAppleWhole } from '@fortawesome/free-solid-svg-icons'
+import { faBorderAll } from '@fortawesome/free-solid-svg-icons'
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+import { faGamepad } from '@fortawesome/free-solid-svg-icons'
+import { faPalette } from '@fortawesome/free-solid-svg-icons'
+import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons'
+import { faPaintBrush } from '@fortawesome/free-solid-svg-icons'
+library.add(faCamera, faUser, faMountainSun, faWaveSquare, faAppleWhole, faBorderAll, faCircleInfo, faGamepad, faPalette, faCircleQuestion, faPaintBrush)
 
-createApp(App).use(router).use(Quasar, quasarUserOptions).use(router).mount('#app')
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).use(Quasar, quasarUserOptions).use(router).mount('#app')
