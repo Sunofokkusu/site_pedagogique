21ac579050b7bdcfab52172c77ec69253bee6b73
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-07-04 15:42:16 +0200

    fix(portrait): retour code

--- site_pedagogique/src/components/activities/activitePortrait.vue
+++ site_pedagogique/src/components/activities/activitePortrait.vue
@@ -20,6 +20,21 @@ export default {
       .then((stream) => {
         // Afficher le flux vidéo dans la balise vidéo
         document.querySelector("#webcam").srcObject = stream;
+        // Attendre que la vidéo soit prête
+        document.querySelector("#webcam").onloadedmetadata = () => {
+          // Lire les codes QR à partir du flux vidéo
+          setInterval(() => {
+            const video = document.getElementById("webcam");
+            const canvas = document.createElement("canvas");
+            if (video) {
+              canvas.width = video.videoWidth;
+              canvas.height = video.videoHeight;
+              canvas.getContext("2d").drawImage(video, 0, 0);
+            } else {
+              stream.getVideoTracks()[0].stop();
+            }
+          });
+        };
       })
       .catch((err) => {
         console.error(err);
